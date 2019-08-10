70ca86629f230f7eba918a60eceb54278ef0f471
clsrs <307589359@qq.com>
2019-08-10 13:55:10 +0800

    我的ef

--- EFCoreDemo/Program.cs
+++ EFCoreDemo/Program.cs
@@ -6,7 +6,7 @@ namespace EFCoreDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World啊!");
         }
     }
 }
