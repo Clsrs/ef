cd10b6bd18611b0e538b75af923fbd7b6bc4e354
clsrs <307589359@qq.com>
2019-08-10 14:05:41 +0800

    修改

--- EFCoreDemo/Program.cs
+++ EFCoreDemo/Program.cs
@@ -6,7 +6,7 @@ namespace EFCoreDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlda!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
